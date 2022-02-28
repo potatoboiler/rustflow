7f9220712f18ee5c6890366f80b2a189d288f0d5
potatoboiler <1nova2hunter3@gmail.com>
2022-02-28 13:37:59 -0800

    Impl atomic ops for executor, partial worker loop

--- src/executor.rs
+++ src/executor.rs
@@ -1,7 +1,58 @@
+use std::{
+    collections::HashMap, hash::Hash, sync::atomic::AtomicU32, sync::atomic::Ordering::Relaxed,
+};
+
+use crate::task_graph::TaskGraph;
 // executor can take workers and assign them to nodes --> invoke(worker, node)
 // find topological sort? (is there any way to have this weighted?)
 
-
 // runtime is a separate class?
 // each thread in the pool has a work queue that can get stolen from
-mod executor {}
+mod worker;
+use worker::Worker;
+
+#[derive(Eq, Hash)]
+enum Domain {
+    GPU,
+    CPU,
+}
+impl PartialEq for Domain {
+    fn eq(&self, other: &Self) -> bool {
+        self == other
+    }
+}
+enum ActThi {
+    Actives,
+    Thieves,
+}
+
+trait Atomic {
+    fn AtomInc(&mut self, a: ActThi, d: &Domain) -> u32;
+    fn AtomDec(&mut self, a: ActThi, d: &Domain) -> u32;
+}
+impl Atomic for Scheduler {
+    fn AtomInc(&mut self, a: ActThi, d: &Domain) -> u32 {
+        let map: &mut HashMap<Domain, AtomicU32> = match a {
+            ActThi::Actives => &mut self.actives,
+            ActThi::Thieves => &mut self.thieves,
+        };
+        map.get_mut(d).unwrap().fetch_add(1, Relaxed) + 1
+    }
+    fn AtomDec(&mut self, a: ActThi, d: &Domain) -> u32 {
+        let map: &mut HashMap<Domain, AtomicU32> = match a {
+            ActThi::Actives => &mut self.actives,
+            ActThi::Thieves => &mut self.thieves,
+        };
+        map.get_mut(&d).unwrap().fetch_sub(1, Relaxed) - 1
+    }
+}
+struct Scheduler {
+    workers: Vec<Worker>,
+    actives: HashMap<Domain, AtomicU32>,
+    thieves: HashMap<Domain, AtomicU32>,
+    // activesLock:
+}
+struct Executor {
+    graph: TaskGraph,
+    scheduler: Scheduler,
+}
--- src/executor/worker.rs
+++ src/executor/worker.rs
@@ -1,12 +1,16 @@
 use crate::{task::Task, task_queue::TaskQueue};
 
-struct Worker {
-    stop: bool,
+use super::Domain;
+pub struct Worker {
+    _stop: bool,
     // t: task::Task,
     queue: TaskQueue,
+    dom: Domain,
     // thread runner : can be CPU or GPU or other
 }
 
+// cited algorithm in paper
+/*
 fn abp_worker_loop(w: &mut Worker) {
     let mut t: Option<Task> = None;
     while !w.stop {
@@ -16,6 +20,26 @@ fn abp_worker_loop(w: &mut Worker) {
         }
         t = w.queue.pop_back();
     }
+}
+*/
+
+// actually does stuff
+fn actual_worker_loop(w: &mut Worker) {
+    let mut t: Option<Task> = None;
+    loop {
+        exploit_task(w, &t);
+        if !wait_for_task(w, &t) {
+            break;
+        }
+    }
+}
+fn exploit_task(w: &mut Worker, t: &Option<Task>) {
+    if let Task = t {
+        if (true) {}
+    }
+}
+fn wait_for_task(w: &mut Worker, t: &Option<Task>) -> bool {
+    false
 }
 
 fn execute_task(w: &mut Worker, t: &Task) {
